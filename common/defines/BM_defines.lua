a74df4e3ef278ecb15089d6087f51713eef5b356
ConferenceCaller <114789362+ConferenceCaller@users.noreply.github.com>
2023-04-30 07:31:26 +0200

    Working version of BM Mod

---
+++ common/defines/BM_defines.lua
@@ -0,0 +1,29 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+-- N MILITARY STUFFS
+--Fortress upkeep
+NDefines.NMilitary.FORTRESS_COST = 0.7
+
+--Insufficient support to positive
+NDefines.NMilitary.INSUFFICIENT_SUPPORT = 0
+
+NDefines.NMilitary.CAVALRY_SPEED = 1.0
+
+--N AI STUFFS
+--AI amount of cav
+NDefines.NAI.MIN_CAV_PERCENTAGE = 10
+NDefines.NAI.MAX_CAV_PERCENTAGE = 75
+
+NDefines.NAI.GOVERNING_CAPACITY_OVER_PERCENTAGE_TOLERATED = 10.0
+
+--N COUNTRY STUFFS
+--Months per dev
+NDefines.NCountry.MONTHS_TO_CHANGE_CULTURE = 7
+
+--Dev cost bs
+NDefines.NCountry.CULTURE_COST_DIFF_ORIGINAL = -50
+NDefines.NCountry.CULTURE_COST_DIFF_ADJACENT = -25
+NDefines.NCountry.CULTURE_COST_DIFF_PRIMARY = 0
+
